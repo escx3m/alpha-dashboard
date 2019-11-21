9941fc6f7a60f5ec5824a2f4b83f273c1d6414e8
m.narminov <m.narminov@gmail.com>
2019-11-21 14:52:11 +0300

    show range of date

--- src/views/DatePicker/rangepickerANTD.js
+++ src/views/DatePicker/rangepickerANTD.js
@@ -1,52 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { DatePicker } from 'antd';
 import 'moment/locale/ru';
+import moment from 'moment';
 
-
-const {  RangePicker } = DatePicker;
+const { RangePicker } = DatePicker;
 
 const StyledRangePicker = styled(RangePicker)`
-    .ant-input-lg {
-        height: 50px;
-        padding: 12px 11px;
-        font-size: 16px;
-        border-color: #3f51b5;
-        color: black;
-        background: white;
-}
-    i.anticon.anticon-calendar.ant-calendar-picker-icon {
-        color: #3f51b5;
-}
-    span.ant-calendar-range-picker-separator {
-        color: black;
-}
-    .ant-calendar-range-picker-input:placeholder-shown {
-        font-size: 18px;
-}
-`
-
-class RangePickerANTD extends React.Component {
-  state = {
-    size: 'large',
-  };
-
-  handleSizeChange = e => {
-    this.setState({ size: e.target.value });
-  };
-
-  render() {
-    const { size } = this.state;
-    return (
-      <div>
-        <StyledRangePicker
-            size={size} 
-            format="DD-MM-YYYY"
-            placeholder={['НАЧАЛО', 'КОНЕЦ']}
-        />
-      </div>
-    );
+  .ant-input-lg {
+    height: 50px;
+    padding: 12px 11px;
+    font-size: 16px;
+    border-color: #3f51b5;
+    color: black;
+    background: white;
   }
+  i.anticon.anticon-calendar.ant-calendar-picker-icon {
+    color: #3f51b5;
+  }
+  span.ant-calendar-range-picker-separator {
+    color: black;
+  }
+  .ant-calendar-range-picker-input:placeholder-shown {
+    font-size: 18px;
+  }
+`;
+
+const RangePickerANTD = (props) => {
+  const { startDay, endDay, setStartDay, setEndDay } = props;
+  const  [size, setSize] = useState('large');
+
+  const handleSizeChange = e => {
+    setSize(e.target.value);
+  };
+  const handleDateChange = val => {
+    setStartDay(val[0]);
+    setEndDay(val[1]);
+    return (
+      [moment(startDay), moment(endDay)]
+    );
+  };
+
+  return (
+    <div>
+      <StyledRangePicker
+        size={size}
+        format="DD-MM-YYYY"
+        placeholder={['НАЧАЛО', 'КОНЕЦ']}
+        value={startDay && [moment(startDay), moment(endDay)]}
+      />
+    </div>
+  );
 }
+
 export default RangePickerANTD;
