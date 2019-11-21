6e8120ad38d31c64aad1127aa99ae2298cb09857
escx3m <ololoataked@gmail.com>
2019-11-21 14:47:21 +0300

    ADD monitoring car

---
+++ src/views/MonitoringCar/carsLoad/CarsLoadTable.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TableContent from '../carsLoad/carsLoadTable/TableContent';
+import TableHeader from '../carsLoad/carsLoadTable/TableHeader';
+import { Grid, CircularProgress } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    marginTop: '20px',
+    textAlign:'center',
+    }
+}));
+
+const CarsLoadTable = ({ cars, loading, selectedWeekStart, checkState }) => {
+  const classes = useStyles();
+  return (
+  loading 
+  ? <CircularProgress className={classes.progress} style={{textAlign:'center'}} />
+  : <Grid container direction="row">
+      <Grid container item xs={12}>
+        <TableHeader 
+          selectedWeekStart={selectedWeekStart}
+        />
+      </Grid>
+      <Grid container item xs={12}>
+        <TableContent 
+          cars={cars}
+          selectedWeekStart={selectedWeekStart}
+          checkState={checkState}
+        />
+      </Grid>
+    </Grid>
+  )
+  };
+
+export default CarsLoadTable;
