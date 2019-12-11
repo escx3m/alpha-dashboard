791e2e5bcc638cc1a024011c08d165b1bf47bd38
m.narminov <m.narminov@gmail.com>
2019-12-11 16:49:19 +0300

    fix start of day(end)

--- src/views/Sms/Sms.js
+++ src/views/Sms/Sms.js
@@ -12,7 +12,7 @@ import { Grid, Card, CardContent, CardHeader, Paper,
   ExpansionPanelDetails as ExpansionBody
 } from '@material-ui/core';
 import { 
-  isSameDay, differenceInHours, 
+  isSameDay, differenceInHours, startOfDay, endOfDay,
   startOfToday, endOfToday } from 'date-fns';
 import { ApiContext } from '../../Routes';
 
@@ -115,7 +115,7 @@ const Sms = () => {
 
   useEffect(() => {
     setLoading(true);
-    api.getRoutes(selectedDate, selectedDate)
+    api.getRoutes(startOfDay(selectedDate), endOfDay(selectedDate))
       .then(res => {
         const routes = res.data;
         const uniqueRoutes = routes.reduce((acc, route) => {
