4d109d736a57d658bb3cde782f29fa1a1ab12c0e
escx3m <ololoataked@gmail.com>
2020-01-11 12:45:30 +0300

    ruLocale Calendar

--- src/views/Sms/Sms.js
+++ src/views/Sms/Sms.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Calendar, useStaticState } from '@material-ui/pickers';
+import {
+  Calendar,
+  useStaticState,
+  MuiPickersUtilsProvider,
+  DatePicker
+} from '@material-ui/pickers';
 import CachedIcon from '@material-ui/icons/Cached';
 import {
   cityShortNames as citiesName,
@@ -13,7 +18,7 @@ import {
   CardContent,
   CardHeader,
   Paper,
-  IconButton,
+  IconButton
 } from '@material-ui/core';
 import {
   isSameDay,
@@ -25,6 +30,18 @@ import {
 } from 'date-fns';
 import { ApiContext } from '../../Routes';
 import ExpansionParent from './Expansion/expansionParent';
+import ruLocale from 'date-fns/locale/ru';
+import { RuLocalizedUtils } from '../../helpers/helpers';
+
+const localeMap = {
+  ru: ruLocale
+};
+const localeFormatMap = {
+  ru: 'd MMM yyyy'
+};
+const localeUtilsMap = {
+  ru: RuLocalizedUtils
+};
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -40,6 +57,7 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Sms = () => {
+  const [locale, setLocale] = useState('ru');
   const classes = useStyles();
   const [openAll, setOpenAll] = useState(true);
   const [loading, setLoading] = useState(false);
@@ -157,10 +175,16 @@ const Sms = () => {
           xs={3}
         >
           <Paper>
-            <Calendar
-              style={{ overflow: 'hidden' }}
-              {...pickerProps}
-            />
+            <MuiPickersUtilsProvider
+              locale={localeMap[locale]}
+              utils={localeUtilsMap[locale]}
+            >
+              <Calendar
+                format={localeFormatMap[locale]}
+                style={{ overflow: 'hidden' }}
+                {...pickerProps}
+              />
+            </MuiPickersUtilsProvider>
           </Paper>
         </Grid>
         <Grid
